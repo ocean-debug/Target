553ecf2cc47d858a43aa0af8e359993817327116
ocean-debug <59615229+ocean-debug@users.noreply.github.com>
2026-08-08 11:50:28 +0800

    p2.6 fix: methods truncation referenced bare _MAX_METHOD_CHARS

--- src/target_agent/pattern_extraction.py
+++ src/target_agent/pattern_extraction.py
@@ -217,7 +217,7 @@ class EuropePmcMetaFetcher:
             doi=(row.get("doi") or "").strip() or None,
             pmcid=pmcid,
             abstract=abstract,
-            source_text=source_text[: self._MAX_METHOD_CHARS + 2000] if source_material == "abstract" else source_text[:_MAX_METHOD_CHARS],
+            source_text=source_text[: self._MAX_METHOD_CHARS + 2000] if source_material == "abstract" else source_text[: self._MAX_METHOD_CHARS],
             source_material=source_material,
         )
 
